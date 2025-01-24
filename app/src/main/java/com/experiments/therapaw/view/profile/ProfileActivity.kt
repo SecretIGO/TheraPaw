4abf24415b5b38d6414e8f409d08d25d23bf5ad0
SecretIGO <110442934+SecretIGO@users.noreply.github.com>
2025-01-24 12:16:51 +0800

    [Create Base UI] Added SignInActivity Added SignUpActivity Added base template for ProfileActivity Added Adapter and ViewModel for the recyclerView Card

--- app/src/main/java/com/experiments/therapaw/view/profile/ProfileActivity.kt
+++ app/src/main/java/com/experiments/therapaw/view/profile/ProfileActivity.kt
@@ -2,23 +2,125 @@ package com.experiments.therapaw.view.profile
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.experiments.therapaw.R
+import com.experiments.therapaw.databinding.ActivityProfileBinding
+import com.experiments.therapaw.view.profile.adapters.ProfileAdapter
+import kotlin.math.abs
 
 class ProfileActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityProfileBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        bind()
+    }
+
+    private fun bind(){
+        bindRecyclerView()
+
+        with(binding){
+            btnBack.setOnClickListener{
+                finish()
+            }
         }
+    }
+
+    private fun bindRecyclerView(){
+        with(binding){
+            val layoutManager = LinearLayoutManager(this@ProfileActivity, LinearLayoutManager.HORIZONTAL, false)
+            rvProfiles.layoutManager = layoutManager
+            rvProfiles.adapter = ProfileAdapter()
+
+            val cardWidth = 225.dpToPx(this@ProfileActivity)
+            val padding = cardWidth / 2
+
+            rvProfiles.setPadding(padding, 0, padding, 0)
+            rvProfiles.clipToPadding = false
+
+            val snapHelper = LinearSnapHelper()
+            snapHelper.attachToRecyclerView(rvProfiles)
+
+            rvProfiles.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+                override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                    super.onScrollStateChanged(recyclerView, newState)
+                    scaleCenterItem(recyclerView)
+                }
+
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    scaleCenterItem(recyclerView)
+                }
+            })
+
+            rvProfiles.post {
+                if ((rvProfiles.adapter?.itemCount ?: 0) > 2) {
+                    rvProfiles.scrollToPosition(1)
+                    scaleCenterItem(rvProfiles)
+                }
+            }
+        }
+    }
+
+    private fun scaleCenterItem(recyclerView: RecyclerView) {
+        val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+        val center = recyclerView.width / 2
+
+        for (i in 0 until layoutManager.childCount) {
+            val child = layoutManager.getChildAt(i) ?: continue
+            val childCenter = (child.left + child.right) / 2
+            val distanceFromCenter = abs(center - childCenter).toFloat()
+            val scale: Float
+            val opacity: Float
+
+            when (layoutManager.getPosition(child)) {
+                0 -> {
+                    scale = 0.5f + 0.5f * (1 - distanceFromCenter / center)
+                    opacity = 0.5f + 0.5f * (1 - distanceFromCenter / center)
+                    if (layoutManager.itemCount <= 2){
+                        child.background?.setTint(resources.getColor(R.color.primary))
+                    } else {
+                        child.background?.setTint(resources.getColor(R.color.backgroundRegular))
+                    }
+                }
+                1 -> {
+                    scale = 0.5f + 0.5f * (1 - distanceFromCenter / center)
+                    opacity = 0.5f + 0.5f * (1 - distanceFromCenter / center)
+                    if (layoutManager.itemCount <= 2){
+                        child.background?.setTint(resources.getColor(R.color.backgroundRegular))
+                    } else {
+                        child.background?.setTint(resources.getColor(R.color.primary))
+                    }
+                }
+                else -> {
+                    scale = 0.5f + 0.5f * (1 - distanceFromCenter / center)
+                    opacity = 0.5f + 0.5f * (1 - distanceFromCenter / center)
+                    child.background?.setTint(resources.getColor(R.color.backgroundCold))
+                }
+            }
+
+            child.scaleX = scale
+            child.scaleY = scale
+            child.alpha = opacity.coerceIn(0.3f, 1.0f)
+        }
+    }
+
+
+    fun Int.dpToPx(context: Context): Int {
+        return (this * context.resources.displayMetrics.density).toInt()
     }
 
     companion object {
---
+++ app/src/main/java/com/experiments/therapaw/view/profile/adapters/ProfileAdapter.kt
@@ -0,0 +1,79 @@
+package com.experiments.therapaw.view.profile.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.experiments.therapaw.R
+
+class ProfileAdapter : RecyclerView.Adapter<ProfileAdapter.CardViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.rvitem_profilecard, parent, false)
+        return CardViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolder, position: Int) {
+        val drawableResource = when {
+            itemCount <= 2 -> {
+                when (position) {
+                    0 -> R.drawable.ic_launcher_foreground
+                    1 -> R.drawable.ico_add
+                    else -> null
+                }
+            }
+            itemCount > 2 -> {
+                when (position) {
+                    0 -> R.drawable.ico_add
+                    1 -> R.drawable.ic_launcher_foreground
+                    else -> null
+                }
+            }
+            else -> null
+        }
+
+        val imageView = holder.itemView.findViewById<ImageView>(R.id.card_profile)
+
+        if (itemCount <= 2) {
+            if(position == 1){
+                val params = imageView.layoutParams as ConstraintLayout.LayoutParams
+                params.width = 50.dpToPx(holder.itemView.context)
+                params.height = 50.dpToPx(holder.itemView.context)
+                imageView.layoutParams = params
+
+                imageView.setColorFilter(
+                    ContextCompat.getColor(holder.itemView.context, R.color.ico_primary),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+            }
+        } else if (itemCount > 2) {
+            if(position == 0){
+                val params = imageView.layoutParams as ConstraintLayout.LayoutParams
+                params.width = 50.dpToPx(holder.itemView.context)
+                params.height = 50.dpToPx(holder.itemView.context)
+                imageView.layoutParams = params
+
+                imageView.setColorFilter(
+                    ContextCompat.getColor(holder.itemView.context, R.color.ico_primary),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+            }
+        }
+
+        if (drawableResource != null) {
+            imageView.setImageResource(drawableResource)
+        }
+    }
+
+    fun Int.dpToPx(context: Context): Int {
+        return (this * context.resources.displayMetrics.density).toInt()
+    }
+
+    override fun getItemCount(): Int = 3
+
+    class CardViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
